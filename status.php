2bd5ef85b5f0fd6aafdebaa15d91ecc1d2432c5e
Stuffmaker <96948850+MrStuffmaker@users.noreply.github.com>
2024-09-09 17:02:20 +0200

    Create status.php

---
+++ status.php
@@ -0,0 +1,28 @@
+<?php
+function get_http_status($url) {
+    $headers = @get_headers($url);
+    return substr($headers[0], 9, 3);
+}
+
+function ping($host, $port = 80, $timeout = 10) {
+    $starttime = microtime(true);
+    $file      = @fsockopen($host, $port, $errno, $errstr, $timeout);
+    $stoptime  = microtime(true);
+    $status    = 0;
+
+    if (!$file) {
+        $status = -1;  // Site down
+    } else {
+        fclose($file);
+        $status = ($stoptime - $starttime) * 1000;
+        $status = floor($status);
+    }
+    return $status;
+}
+
+$url = "https://stuffmaker.net";
+$host = parse_url($url, PHP_URL_HOST);
+
+echo "HTTP Status: " . get_http_status($url) . "\n";
+echo "Ping: " . ping($host) . " ms\n";
+?>
